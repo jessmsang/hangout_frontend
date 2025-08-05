40156bcfbe69f91f4724f92c1b1d034612673476
Jess Sang <jessicamsang@hotmail.com>
2025-08-05 11:14:25 -0400

    rename root folder to specify frontend and add gitignore, eslint, prettierignore

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Header from "../Header/Header";
+
+export default function App() {
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import "./Logo.css";
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <button className="logo__btn">Hangout</button>
+    </div>
+  );
+}
