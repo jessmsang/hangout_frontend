d1c71fa653a595731b53a790414674e330d9ac50
Jess Sang <jessicamsang@hotmail.com>
2025-09-23 12:49:19 -0400

    update Router basename to '/'

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./components/App/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/hangout_frontend/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>
