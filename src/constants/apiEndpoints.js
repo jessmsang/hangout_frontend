26ef63c29c0e19a1ef56a2d39ec4ee78f8b2a558
Jess Sang <jessicamsang@hotmail.com>
2025-09-23 11:31:46 -0400

    update BASE_URL and homepage

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -1,3 +1,6 @@
 export const weatherAPIkey = "df8913ef0c6c702c8aabdfb11223c20f";
 
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.hangout.devbyjess.com"
+    : "http://localhost:3001";
